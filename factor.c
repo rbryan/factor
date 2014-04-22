a20230978ed463ccb635184d140669dc800f46c2
Russell Bryan <bryanr@asmsa.org>
2014-04-22 16:46:07 -0500

    Arbitrary precision

--- factor.c
+++ factor.c
@@ -1,66 +1,63 @@
+#include <gmp.h>
 #include <mpi.h>
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef unsigned int u128 __attribute__((mode(TI)));
-typedef unsigned int u64 __attribute__((mode(DI)));
-
 
 
 int world_size;
 int world_rank;
 char proc_name[MPI_MAX_PROCESSOR_NAME];
 int proc_name_len;
-u128 randfactor;
+mpz_t randfactor;
 
-inline u128 my_abs(__int128_t a){
-	if(a<0) return (u128)-1*a;
-	return (u128) a;
-}
 
-inline u128 f(u128 i, u128 n){
+void  f(mpz_t out,mpz_t i, mpz_t n){
 //	return (i*i+1)%n;
-	return (i*i+randfactor)%n;
+	mpz_mul(out,i,i);
+	mpz_add(out,out,randfactor);
+	mpz_mod(out,out,n); //maybe problem?
 }
 
-u128 gcd(u128 a, u128 b){
-	if(b==0) return a;
-	else
-		return gcd(b,a%b);
-}
-
-void print128( u128 p){
-	
-	printf("%llu\n",(u64)(p>>64));
-	printf("%llu",(u64)(p));
-}
-
-void prho(u128 n){
-	u128 a;
-	u128 b;
-	u128 p=1;
-	srand(time(NULL)*world_rank);
+void prho(mpz_t n){
+	mpz_t tmp;
+	mpz_t a;
+	mpz_t b;
+	mpz_t p;
+	mpz_init(a);
+	mpz_init(b);
+	mpz_init(p);
+	mpz_init(tmp);
+	mpz_set_str(p,"1",10);
+	gmp_randstate_t r_state;
+	gmp_randinit_default(r_state);
+	gmp_randseed_ui(r_state,time(NULL)*world_rank);
+//	srand(time(NULL)*world_rank);
 //	#pragma omp parallel
 	for(;;){
-		randfactor = ((u128)rand())<<96 | ((u128)rand())<<64 | ((u128)rand())<<32 | ((u128)rand());
-		a = rand()%n;
-		b = a;
-		u64 count=0;
+		mpz_urandomb(randfactor,r_state,mpz_sizeinbase(n,10));
+		mpz_urandomb(randfactor,r_state,mpz_sizeinbase(n,10));
+		mpz_mod(a,a,n);;
+		mpz_set(b,a);
 
 		do{
-			a = f(a,n);
-			b = f(f(b,n),n);
-			p = gcd(my_abs((__int128_t)b-(__int128_t)a),n);	
-			count ++;
-			if(p>1) break;
-		}while(b != a);
+			f(a,a,n);
+			f(b,b,n);
+			f(b,b,n);
+			mpz_sub(tmp,b,a);
+			mpz_abs(tmp,tmp); //may cause problems
+			mpz_gcd(p,tmp,n);
+			if(mpz_cmp_ui(p,1) > 0) break;
+		}while(mpz_cmp(b,a)!=0);
 
-		if(p!=n && p > 1){
+		if(mpz_cmp(p,n) != 0 && mpz_cmp_ui(p,1) > 0){
 			//break;
 			printf("World Rank:\t%d\tProcessor:\t%s\n",world_rank,proc_name);
-			print128(p);
-			print128(p/n);
+			gmp_printf("%Zd\n",p);
+			mpz_t out;
+			mpz_init(out);
+			mpz_div(out,n,p);
+			gmp_printf("%Zd\n",out);
 		}
 	}
 //	return p;
@@ -75,11 +72,10 @@ int main(int argc, char **argv){
 	MPI_Comm_rank(MPI_COMM_WORLD,&world_rank);
 	MPI_Get_processor_name(proc_name,&proc_name_len);
 
-	u128 a =  56919724113ULL;
-		a*=100000000000ULL;
-		a+= 41955564803ULL;//5691972411341955564803 
-		a=73874852881ULL*77048849363ULL;
-		a=45433413203ULL*191244791ULL;
-	print128(a);
-	prho(a);
+	mpz_t n;
+	mpz_init(n);
+	mpz_set_str(n,"5691972411341955564803",10);
+	mpz_set_str(n,"430960000765005008079109",10);
+	//5691972411341955564803 
+	prho(n);
 }
